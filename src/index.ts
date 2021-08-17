22ee72276aca9fd6adfc9d67f307f47a5616e07d
Vlad <chepurnojv@gmail.com>
2021-08-17 22:49:10 +0300

    applied google maps module

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello");
+// import { User } from "./User";
+// import { Company } from "./Company";
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
