b53c023bd0e5d792e74a21565535d8dfa9d85e0e
Vlad <chepurnojv@gmail.com>
2021-08-23 16:26:08 +0300

    completed maps app,

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,11 +1,14 @@
 // import { User } from "./User";
 // import { Company } from "./Company";
 
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
+
+  markerContent(): string;
+  color: string;
 }
 
 export class CustomMap {
@@ -22,12 +25,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 
